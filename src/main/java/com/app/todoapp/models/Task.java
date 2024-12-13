46f73c08db4744b96af9db9032e67f842ffbc0f1
metinbicaksiz <metinbicaksiz@gmail.com>
2024-12-13 21:21:00 +0300

    todo toggle and delete implemented

--- src/main/java/com/app/todoapp/models/Task.java
+++ src/main/java/com/app/todoapp/models/Task.java
@@ -5,13 +5,24 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Data
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String title;
-    private boolean completed;
+    public Long id;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String title;
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public boolean completed;
 }
